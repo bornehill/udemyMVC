04faacbd4e01dedd8e5fe20cd611c47191b96b9f
bornehill <ali.escamilla.navarro@gmail.com>
2017-07-28 17:18:59 -0500

    Contacts controller
    Add contacts controller and DB conection

---
+++ udemyMVC/Models/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace udemyMVC.Models
+{
+	public class Contact
+	{
+		public int ContactId { get; set; }
+		public string Name { get; set; }
+		public string LastName { get; set; }
+		public string Street { get; set; }
+		public string NoInt { get; set; }
+		public string Urbanization { get; set; }
+		public string City { get; set; }
+		public string State { get; set; }
+		public string Country { get; set; }
+		public int ZipCode { get; set; }
+	}
+}
