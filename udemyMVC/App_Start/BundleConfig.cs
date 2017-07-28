ac7574b0c01d9831a616a692e4bc1c24c0680abb
bornehill <ali.escamilla.navarro@gmail.com>
2017-07-28 09:12:50 -0500

    Initial commit
    The first commit

---
+++ udemyMVC/App_Start/BundleConfig.cs
@@ -0,0 +1,29 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace udemyMVC
+{
+	public class BundleConfig
+	{
+		// For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+		public static void RegisterBundles(BundleCollection bundles)
+		{
+			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+									"~/js/jquery.min.js"));
+
+			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+								"~/js/bootstrap.min.js",
+								"~/js/jquery.dataTables.min.js",
+								"~/js/dataTables.bootstrap.js",
+								"~/js/dataTables.colReorder.min.js"));
+
+			bundles.Add(new StyleBundle("~/Content/css").Include(
+								"~/css/bootstrap.min.css",
+								"~/css/jquery.dataTables.min.css",
+								"~/css/jquery.dataTables_themeroller.min.css",
+								"~/css/dataTables.bootstrap.min.css",
+								"~/css/colReorder.dataTables.min.css",
+								"~/css/site.css"));
+		}
+	}
+}
---
+++ udemyMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="udemyMVC.MvcApplication" Language="C#" %>
